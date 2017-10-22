7b1fefd9e1bb13c38e4151ead9f779bff610b855
DorianPopovic <dorian.popovic@hotmail.com>
2017-10-22 11:08:49 +0200

    Changed the main simulation to simulate the connection between two neurons, one with input current that can sen a spike, the other without input current that can receive the spike + new cmake files for the execution.

--- main.cpp
+++ main.cpp
@@ -6,23 +6,28 @@ using namespace std;
 
 
 /** 
- * MAIN SINGLE-NEURON SIMULATION LOOP
- * --> Updates a given neuron during each step time, beginning at t = 0.
+ * MAIN TWO NEURONS CONNECTION SIMULATION LOOP
+ * --> Updates two given neurons during each step time, beginning at t = 0.
  * --> Writes the value of the membrane potential in a file after each time step.
+ * --> Makes the connected neuron deal with spikes, FIRST WITHOUT BUFFER.
  */
 
 int main()
 {
 	/** CHANGE THE SIMULATION TIMES HERE (in integers = *10 the time you want in ms)
-	 *  endtime       --> when does the simulation end ?
-	 *  current_start --> when does the current input start ?
-	 *  current_end   --> when does the current input stop ?
+	 *  endtime          --> when does the simulation end ?
+	 *  current_start    --> when does the current input start ?
+	 *  current_end      --> when does the current input stop ?
+	 *  single_neuron    --> has input current and can send spikes to the other neuron
+	 *  connected_neuron --> no input current, receives spikes from the other neuron
 	 */
 	int endtime = 5000;
 	int current_start = 1000;
 	int current_end = 4000;
 	double I_ext = 1.01;
+	
 	Neuron single_neuron;
+	Neuron connected_neuron;
 	
 	
 	ofstream MembPotFile;  		
@@ -34,9 +39,10 @@ int main()
 	{
 		if( (current_start <= simtime) and (simtime < current_end) )
 		{
-			/** Current input time interval
+			/** Current input time interval ONLY FOR single_neuron who can send spikes
 			 *  --> set I_ext_ to the entered value up here.
 			 *  --> else no current input.
+			 *  --> connected_neuron has no current input during the simulation
 			 */
 			 single_neuron.setI_ext_(I_ext);
 		}
@@ -46,19 +52,31 @@ int main()
 		}
 		
 		/** 
-		 * Updating the neuron for a single time-step :
-		 * --> store the membrane potential in a file, for the connected neuron.
-		 * --> if the neuron spikes during this single time-step, write it in the terminal. */
+		 * Updating the neuron(s) for a single time-step :
+		 * --> store the membrane potential in a file, for the connected_neuron.
+		 * --> if the neuron(s) spike during this single time-step, write it in the terminal. */
 		bool single_Spike = single_neuron.update(1);
-		MembPotFile << "At time t= " << simtime*0.1 << "ms    V= " << single_neuron.getV_() << endl;
+		bool connected_Spike = connected_neuron.update(1);
+		
+		MembPotFile << "At time t= " << simtime*0.1 << "ms    V= " << connected_neuron.getV_() << endl;
 		
 		if (single_Spike)
 		{
-			cout << " SPIKE FROM THE SINGLE NEURON AT t= " << simtime*0.1 << "ms" << endl;
+			/** When the single_neuron spikes
+			 * --> write it in terminal.
+			 * --> connected neuron has to receive the spike (J=0.1).
+			 */
+			 cout << " SPIKE FROM THE SINGLE NEURON AT t= " << simtime*0.1 << "ms" << endl;
+			 connected_neuron.spike_receive(0.1);
+		}
+		if (connected_Spike)
+		{
+			cout << " SPIKE FROM THE CONNECTED NEURON AT t= " << simtime*0.1 << "ms" << endl;
 		}
 	}
 	
-	cout << " Number of spikes for the single_neuron : " << single_neuron.getNum_Spikes_() << endl;
+	cout << " Number of spikes for the single neuron : " << single_neuron.getNum_Spikes_() << endl;
+	cout << " Number of spikes for the connected neuron : " << connected_neuron.getNum_Spikes_() << endl;
 	
 	return 0;
 }
