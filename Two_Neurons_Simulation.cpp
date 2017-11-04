16879458d644d28a6785d74d36a2f786d9b347bf
DorianPopovic <32441218+DorianPopovic@users.noreply.github.com>
2017-11-04 21:01:24 +0100

    Reduced the length using the two neurons simulation method.

--- Two_Neurons_Simulation.cpp
+++ Two_Neurons_Simulation.cpp
@@ -7,6 +7,15 @@ using namespace std;
 
 int main()
 {
+	/*************************************************************************
+	 *   CHANGE THE SIMUALTION CONSTANTS HERE
+	 * 
+	 * -> endtime: when does the simulation end ?
+	 * -> current_start: when does the input begin ?
+	 * -> current_start: when does the input stop ?
+	 * -> I_ext: how much is the external input ?
+	 * -> Delay : how many time does the spike take to travel (1.5 ms for us)
+	 *************************************************************************/
 
 	int endtime = 5000;
 	int current_start = 1000;
@@ -17,40 +26,8 @@ int main()
 	Neuron single_neuron;
 	Neuron connected_neuron;
 	
-	
-	ofstream MembPotFile;  		
-    MembPotFile.open ("./MembPotFile");
-    MembPotFile << "Starting Simulation.\n";
-	
-	
-	for( int simtime=0; simtime < endtime; ++simtime )
-	{
-		if( (current_start <= simtime) and (simtime < current_end) )
-		{
-			 single_neuron.setI_ext_(I_ext);
-		}
-		else
-		{
-			single_neuron.setI_ext_(0.0);
-		}
-		bool single_Spike = single_neuron.update_test(1);
-		bool connected_Spike = connected_neuron.update_test(1);
-		
-		MembPotFile << "At time t= " << simtime*0.1 << "ms    V= " << connected_neuron.getV_() << endl;
-		
-		if (single_Spike)
-		{
-			 cout << " SPIKE FROM THE SINGLE NEURON AT t= " << simtime*0.1 << "ms" << endl;
-			 connected_neuron.spike_receive(simtime+Delay, 0.1);
-		}
-		if (connected_Spike)
-		{
-			cout << " SPIKE FROM THE CONNECTED NEURON AT t= " << simtime*0.1 << "ms" << endl;
-		}
-	}
-	
-	cout << " Number of spikes for the single neuron : " << single_neuron.getNum_Spikes_() << endl;
-	cout << " Number of spikes for the connected neuron : " << connected_neuron.getNum_Spikes_() << endl;
+	single_neuron.simulate_two_neurons(connected_neuron, endtime, current_start, current_end, I_ext, Delay);
+
 	
 	return 0;
 }
