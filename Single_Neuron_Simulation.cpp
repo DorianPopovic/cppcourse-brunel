a1bfcfb2d819c69bcf997d4595fd4ae2e9d03f41
DorianPopovic <32441218+DorianPopovic@users.noreply.github.com>
2017-11-04 20:01:07 +0100

    Made it shorter using the simulation method

--- Single_Neuron_Simulation.cpp
+++ Single_Neuron_Simulation.cpp
@@ -6,38 +6,23 @@ using namespace std;
 
 int main()
 {
+	/***********************************************
+	 * Change the constants for the simulation here
+	 * -> endtime: when does the simulation end ?
+	 * -> current_start: when does the input begin ?
+	 * -> current_start: when does the input stop ?
+	 * -> I_ext: how much is the external input ?
+	 ***********************************************/
+	
 	int endtime = 5000;
 	int current_start = 0;
 	int current_end = 4000;
 	double I_ext = 1.01;
+	
 	Neuron single_neuron;
 	
 	
-	ofstream MembPotFile;  		
-    MembPotFile.open ("./MembPotFile");
-    MembPotFile << "Starting Simulation.\n";
-	
-	
-	for( int simtime=0; simtime < endtime; ++simtime )
-	{
-		if( (current_start <= simtime) and (simtime < current_end) )
-		{
-			 single_neuron.setI_ext_(I_ext);
-		}
-		else
-		{
-			single_neuron.setI_ext_(0.0);
-		}
-		bool single_Spike = single_neuron.update_test(1);
-		MembPotFile << "At time t= " << simtime*0.1 << "ms    V= " << single_neuron.getV_() << endl;
-		
-		if (single_Spike)
-		{
-			cout << " SPIKE FROM THE SINGLE NEURON AT t= " << simtime*0.1 << "ms" << endl;
-		}
-	}
-	
-	cout << " Number of spikes for the single_neuron : " << single_neuron.getNum_Spikes_() << endl;
+	single_neuron.simulate_one_neuron(endtime, current_start, current_end, I_ext);
 	
 	return 0;
 }
