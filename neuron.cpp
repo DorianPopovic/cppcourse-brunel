53ed58b8a6a2e77f6759fd15e17f627145a322e6
DorianPopovic <32441218+DorianPopovic@users.noreply.github.com>
2017-11-04 19:59:16 +0100

    Added a method to simulate the entire neuron

--- neuron.cpp
+++ neuron.cpp
@@ -1,15 +1,22 @@
 #include "neuron.h"
 #include <cmath>
 #include <random>
-
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 
-//------------------CONSTRUCTOR----------------//
+/************************************************** 
+ * 	   	   CONSTRUCTOR
+*************************************************/
 
 Neuron:: Neuron()
     :
+    /**
+    * Converting the steps for the refractory
+    * period to have an unsigned long
+    **/
     RefSteps_ (static_cast<const unsigned long>( RefTime_ / h_)),
     V_(0.0),
     I_ext_(0.0),
@@ -21,12 +28,24 @@ Neuron:: Neuron()
     {
 		C1_ = exp(-h_ / Tau_);
 		C2_ = R_*(1.0 - C1_);
-		DelaySteps_ = static_cast<unsigned long>(ceil(Delay_/h_)); //Computes the smallest integer value not less than Delay_/h_
-		Buffer_.resize(DelaySteps_ + 1, 0.0);
+		
+		/***
+		 * Computes the smallest integer value not less than (Delay_/h_)
+		 ***/
+		DelaySteps_ = static_cast<unsigned long>(ceil(Delay_/h_));
+		
+		/***
+		 * Make sur that the buffer has the right size
+		 ***/
+		 Buffer_.resize(DelaySteps_ + 1, 0.0);
 	}
 
 
-//---------------METHODS GIVING ACCESS TO THE ATTRIBUTES-------------//
+
+
+/************************************************** 
+ * 	   	    GETTERS
+*************************************************/
 
 double Neuron::getV_() const
 {
@@ -55,12 +74,11 @@ unsigned long Neuron::getClock_() const
 
 
 
-//------------METHODS SETTING THE VALUES FOR THE ATTRIBUTES------------//
-/** 
- *  setV_               ---> Sets the membrane potential to @param v.
- *  setI_ext_           ---> Sets the external current to @param i.
- *  setLast_Spike_time_ ---> Sets the time of the last occurin spike to @param T.  
- */
+
+
+/************************************************** 
+ * 	   	    SETTERS
+*************************************************/
 
 void Neuron::setV_(double v)
 {
@@ -80,18 +98,19 @@ void Neuron::setLast_Spike_time_(unsigned long T)
 
 
 
-//-----------------------UPDATE METHOD----------------------//
-/** 
- * Updates our neuron in time for how many simulation steps we want.
- * @param simsteps    --> for how many simulation steps do we want to update our neuron
- * @return bool spike --> returns TRUE if there was a spike during "simsteps" steps
- *                    --> returns FALSE if there were no spikes during the steps. 
- */
+/************************************************** 
+ * 	          UPDATE METHODS
+*************************************************/
+
 bool Neuron::update(unsigned long simsteps)
 {
 	if (simsteps == 0) return false;
 	
 	bool spike = false;
+	
+	/***
+	* How many steps until we stop the updating
+	***/
 	const auto stoptime_ = Clock_ + simsteps;
 	
 	
@@ -100,38 +119,48 @@ bool Neuron::update(unsigned long simsteps)
 		const auto IN = Clock_ % (DelaySteps_ + 1);
 		if ( Vth_ < V_ ) 
 		{
-			/**
-			 * Spike occurs :
-			 * --> increment the number of spikes.
-			 * --> time of the last spike is the internal clock time.
-			 */
-			spike = true;
-			++Num_Spikes_;
-			Last_Spike_time_ = Clock_;
+			 /***
+			 *Spike occurs :
+			 *  --> increment the number of spikes.
+			 *  --> time of the last spike is the internal clock time.
+			 ***/
+			 spike = true;
+			 ++Num_Spikes_;
+			 Last_Spike_time_ = Clock_;
 			
 		}
 		if ((Last_Spike_time_ > 0 ) and ((Clock_ - Last_Spike_time_) < RefSteps_)) 
 		{
-			/** 
-			 * REFRACTORY PERIOD :
-			 * --> set the membrane potential to 0 or 10.
-			 */
+			 /***
+			 * Refractory period is until we didn't 
+			 * pass the last spike time by (RefSteps)
+			 ***/
 			 V_ = 0.0;
 		}
 		else
 		{
-			/**
-			 * No spike and not refractory :
-			 * --> Solve the membrane potential differential equation using the constants (see constructor).
-			 * --> I_ext_ will be checked and set in the main loop.
-			 */
-				static random_device rd;
-				static mt19937 gen(rd());
-				static poisson_distribution<> Ext_Rate_(2);
 			 
-			 V_ = C1_*V_ + I_ext_*C2_ + Buffer_[IN] + Ext_J_*Ext_Rate_(gen);
+			  /***
+			   * generating the random devices for
+			   * the poisson distribution calculation 
+			   ***/
+			   static random_device rd;
+			   static mt19937 gen(rd());
+			   static poisson_distribution<> Ext_Rate_(2);
+			   
+			  /***
+			   * We solve equation for the membrane potential
+			   *   --> using the constants computed earlier
+			   *   --> adding what is in the buffer
+			   *   --> generating random external input using poisson distribution
+			   ***/
+			   V_ = C1_*V_ + I_ext_*C2_ + Buffer_[IN] + Ext_J_*Ext_Rate_(gen);
 		}
 		
+	    /***
+	     * don't forget to clean buffer and 
+	     * increment clock at each time steps
+	     ***/
 	    Buffer_[IN] = 0.0;
 	    ++Clock_;
 	}
@@ -140,7 +169,10 @@ bool Neuron::update(unsigned long simsteps)
 
 
 
-
+/***
+ * Same function as update function excpet there is no random external input.
+ * Use for the google tests to still work after we add poisson distribution.
+***/
 bool Neuron::update_test(unsigned long simsteps)
 {
 	if (simsteps == 0) return false;
@@ -154,34 +186,17 @@ bool Neuron::update_test(unsigned long simsteps)
 		const auto IN = Clock_ % (DelaySteps_ + 1);
 		if ( Vth_ < V_ ) 
 		{
-			/**
-			 * Spike occurs :
-			 * --> increment the number of spikes.
-			 * --> time of the last spike is the internal clock time.
-			 */
 			spike = true;
 			++Num_Spikes_;
 			Last_Spike_time_ = Clock_;
-			
 		}
 		if ((Last_Spike_time_ > 0 ) and ((Clock_ - Last_Spike_time_) < RefSteps_)) 
 		{
-			/** 
-			 * REFRACTORY PERIOD :
-			 * --> set the membrane potential to 0 or 10.
-			 */
 			 V_ = 0.0;
 		}
 		else
 		{
-			/**
-			 * No spike and not refractory :
-			 * --> Solve the membrane potential differential equation using the constants (see constructor).
-			 * --> I_ext_ will be checked and set in the main loop.
-			 */
-			 
-			
-			 V_ = C1_*V_ + I_ext_*C2_ + Buffer_[IN];
+			V_ = C1_*V_ + I_ext_*C2_ + Buffer_[IN]; 
 		}
 		
 	    Buffer_[IN] = 0.0;
@@ -194,18 +209,56 @@ bool Neuron::update_test(unsigned long simsteps)
 
 
 
-//---------------------METHOD TO RECEIVE SPIKES----------------//
-/** Updates our neuron when it receives a spike from another neuron
- *   FIRST WITHOUT THE DELAY --> membrane potential goes up immediately after the spike
- *   SECOND WITH DELAY       --> delay for the time it takes the spike to travel has to be taken into account
- *   FIRST : @param J --> by how much does V go up when it receives a spike
- *                    --> J usually equals 0.1, but it can change depending on the conections
- *   
- *   SECOND: @param t --> time when the spike should be received
- *                    --> internal clock of the neuron sending the spike + delay for the travel
- */
+/************************************************** 
+ * 	    METHOD TO RECEIVE THE SPIKES
+*************************************************/
+
 void Neuron::spike_receive(unsigned long t, double J)
 {
+	/***
+	 * Received spike will be received (OUT)
+	 * after 15 steps so we put it into buffer[16]
+	 * and when we reach that buffer index in
+	 * the update method we take it out
+	 ***/
 	const size_t OUT = t % (DelaySteps_ + 1);
-	Buffer_[OUT] += j;
+	Buffer_[OUT] += J;
+}
+
+
+
+
+/******************************************************************************************************** 
+ * 	    						METHOD THAT UPDATES THE NEURON
+*********************************************************************************************************/
+void Neuron::simulate_one_neuron(int endtime, int current_start, int current_end, double I_ext)
+{
+	
+	ofstream MembPotFile;  		
+    MembPotFile.open ("./MembPotFile");
+    MembPotFile << "Starting Simulation.\n";
+	
+	
+	for( int simtime=0; simtime < endtime; ++simtime )
+	{
+		if( (current_start <= simtime) and (simtime < current_end) )
+		{
+			 setI_ext_(I_ext);
+		}
+		else
+		{
+			setI_ext_(0.0);
+		}
+		
+		bool single_Spike = update_test(1);
+		MembPotFile << "At time t= " << simtime*0.1 << "ms    V= " << getV_() << endl;
+		
+		if (single_Spike)
+		{
+			cout << " SPIKE FROM THE SINGLE NEURON AT t= " << simtime*0.1 << "ms" << endl;
+		}
+	}
+	
+	MembPotFile.close();
+	cout << " Number of spikes for the single_neuron : " << getNum_Spikes_() << endl;
 }
